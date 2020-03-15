6b4d97d56358361eaddc48a6ff87bef48222934e
ahdelron <kthday107@gmail.com>
2020-03-15 21:24:43 +0900

    [Feat] LyricsControlXaml 코드 적용 및 LyricsSceneXaml 제작 시작

--- LyricsSceneMaker_CSharp/LyricsControlXaml.xaml.cs
+++ LyricsSceneMaker_CSharp/LyricsControlXaml.xaml.cs
@@ -2,6 +2,7 @@
 using NAudio.Wave;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -26,7 +27,6 @@ namespace LyricsSceneMaker_CSharp
         private AudioFileReader audioFile;
         private Song song;
         private int notesNowSelectedIndex = 0;
-        private int formEffectNowSelectedIndex = 0;
 
         // 특수효과 키 추가하려면 이 변수를 수정하면 됨.
         private Keys[] noteTrigger = { Keys.Enter, Keys.Space };
@@ -35,6 +35,8 @@ namespace LyricsSceneMaker_CSharp
         public LyricsControlXaml()
         {
             InitializeComponent();
+            LyricsSceneXaml scene = new LyricsSceneXaml();
+            scene.Show();
         }
 
         // 음악파일 불러오기
@@ -72,13 +74,7 @@ namespace LyricsSceneMaker_CSharp
             string[] lines_lyrics = LyricsTextBox.Text.Split('\n');
 
             // 모드 변환 (곡 정보 입력 창 -> 가사 싱크 맞추는 폼)
-            //ControlCollection controls = this.Controls;
-
-            //foreach (Control control in controls)
-            //{
-            //    control.Left -= 380;
-            //}
-
+            // 컨트롤 좌측 이동 및 크기 작아지게
             ArtistTextBox.IsEnabled = false;
             SongNameTextBox.IsEnabled = false;
             SelectFile.IsEnabled = false;
@@ -86,15 +82,21 @@ namespace LyricsSceneMaker_CSharp
             InitializeButton.IsEnabled = false;
             LyricsLoadButton.IsEnabled = false;
             LyricsSaveButton.IsEnabled = true;
-
+            Pause.IsEnabled = true;
+            Play.IsEnabled = true;
+            Replay.IsEnabled = true;
+            NotesListBox.IsEnabled = true;
+            NotesLoadButton.IsEnabled = true;
+            NotesSaveButton.IsEnabled = true;
+            
             // Song 개체 생성
             song = new Song(SongNameTextBox.Text, ArtistTextBox.Text, SelectFile.Content.ToString(), lines_lyrics);
             notesNowSelectedIndex = 0;
-            formEffectNowSelectedIndex = 0;
+            //formEffectNowSelectedIndex = 0;
 
             // Scene 폼에 곡 이름, 아티스트 정보를 넘겨준다.
-            toscene(0, ArtistTextBox.Text + " - " + SongNameTextBox.Text,
-                (song.Lyrics.Length > 0) ? song.Lyrics[0] : null);
+            //toscene(0, ArtistTextBox.Text + " - " + SongNameTextBox.Text,
+            //    (song.Lyrics.Length > 0) ? song.Lyrics[0] : null);
 
             // 노래 재생하기
             if (outputDevice == null)
@@ -118,16 +120,14 @@ namespace LyricsSceneMaker_CSharp
             audioFile = null;
         }
 
-        // LyricsSceneXaml 폼 호출
-        // To be implemented
-
-        // 텍스트 애니메이션 추가 이벤트
-        private void NotesListBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        // 텍스트 애니메이션 추가 이벤트 // 버튼으로 바꿔야겠음 마우스로 치기
+        private void NotesListBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
+            System.Windows.Forms.MessageBox.Show(e.Key.ToString());
             Boolean isValid = false;
             foreach (Keys key in noteTrigger)
             {
-                if (e.KeyCode == key)
+                if (e.Key.Equals(key))
                 {
                     isValid = true;
                     break;
@@ -140,7 +140,7 @@ namespace LyricsSceneMaker_CSharp
 
                 int insertIndex = 0;
                 long nowTime = audioFile.Position;
-                string insertString = nowTime + "," + (int)e.KeyCode; // opcode
+                string insertString = nowTime + "," + (int)e.Key; // opcode
 
                 if (size == 0)
                 {
@@ -167,6 +167,7 @@ namespace LyricsSceneMaker_CSharp
             }
         }
 
+        // 시점 변경 이벤트
         private void NotesListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (outputDevice == null)
@@ -182,19 +183,25 @@ namespace LyricsSceneMaker_CSharp
             outputDevice.Pause();
 
             int selectedIndex = NotesListBox.SelectedIndex;
+            if(selectedIndex == -1)
+            {
+                System.Windows.Forms.MessageBox.Show("실어");
+                return;
+            }
+
             ListBoxItem selectedItem = (ListBoxItem)NotesListBox.SelectedItem;
 
-            DialogResult dr = System.Windows.MessageBox.Show("선택한 지점부터 재생하시겠습니까?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            DialogResult dr = System.Windows.Forms.MessageBox.Show("선택한 지점부터 재생하시겠습니까?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
             if (dr == System.Windows.Forms.DialogResult.OK)
             {
                 notesNowSelectedIndex = selectedIndex;
                 long nowTime = long.Parse(selectedItem.Content.ToString().Split(',')[0]);
-                formEffectNowSelectedIndex = getFormEffectNowIndex(nowTime);
+                //formEffectNowSelectedIndex = getFormEffectNowIndex(nowTime);
                 audioFile.Position = nowTime;
             }
             else
             {
-                dr = System.Windows.MessageBox.Show("더블 클릭한 노트를 삭제하시겠습니까?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                dr = System.Windows.Forms.MessageBox.Show("더블 클릭한 노트를 삭제하시겠습니까?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
                 if (dr == System.Windows.Forms.DialogResult.OK)
                 {
                     NotesListBox.Items.RemoveAt(selectedIndex);
@@ -204,33 +211,212 @@ namespace LyricsSceneMaker_CSharp
             outputDevice.Play();
         }
 
+        // 가사 불러오기
         private void LyricsLoadButton_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "가사 데이터 파일 (*.txt)|*.txt";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                StreamReader sr = File.OpenText(ofd.FileNames[0]);
 
+                // 노트 데이터 읽어오기
+                if (sr.Peek() > 0)
+                {
+                    sb.Append(sr.ReadToEnd());
+                }
+
+                // 객체 닫기
+                sr.Close();
+
+                // Text 속성 변경
+                LyricsTextBox.Text = sb.ToString();
+            }
         }
 
+        // 가사 저장하기
         private void LyricsSaveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "가사 데이터 파일 (*.txt)|*.txt";
+            sfd.InitialDirectory = @"C:\";
+            sfd.RestoreDirectory = true;
+            sfd.DefaultExt = "notes";
+            sfd.FileName = ArtistTextBox.Text + "-" + SongNameTextBox.Text;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileStream fs = null;
+                StreamWriter sw = null;
+                try
+                {
+                    fs = (FileStream)sfd.OpenFile();
+                    sw = new StreamWriter(fs);
+                    sw.Write(LyricsTextBox.Text);
+                    sw.Close();
+                    fs.Close();
+                    System.Windows.Forms.MessageBox.Show("저장 성공!", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (IOException e1)
+                {
+                    if (fs != null) fs.Close();
+                    if (sw != null) fs.Close();
+                    System.Windows.Forms.MessageBox.Show("저장 실패!", "Fatal Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Console.Write("{0}", e1.StackTrace);
+                }
+            }
         }
 
-        
+        // 가사 노트 저장하기 버튼
+        private void NotesSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (ListBoxItem item in NotesListBox.Items)
+            {
+                string line_data = item.Content.ToString() + "|";
+                sb.Append(line_data);
+            }
 
-        
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "노트 데이터 파일 (*.notes)|*.notes";
+            sfd.InitialDirectory = @"C:\";
+            sfd.RestoreDirectory = true;
+            sfd.DefaultExt = "notes";
+            sfd.FileName = ArtistTextBox.Text + "-" + SongNameTextBox.Text;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileStream fs = null;
+                StreamWriter sw = null;
+                try
+                {
+                    fs = (FileStream)sfd.OpenFile();
+                    sw = new StreamWriter(fs);
+                    sw.Write(sb.ToString());
+                    sw.Close();
+                    fs.Close();
+                    System.Windows.Forms.MessageBox.Show("저장 성공!", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (IOException e1)
+                {
+                    if (fs != null) fs.Close();
+                    if (sw != null) fs.Close();
+                    System.Windows.Forms.MessageBox.Show("저장 실패!", "Fatal Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Console.Write("{0}", e1.StackTrace);
+                }
+            }
+        }
 
+        // 가사 노트 불러오기 버튼
+        private void NotesLoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult dr;
+            if (NotesListBox.Items.Count != 0)
+            {
+                dr = System.Windows.Forms.MessageBox.Show("정말로 진행하시던 것을 삭제하고\r\n새로운 노트 정보를 덮어씌우시겠습니까?", "Question",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if (dr != System.Windows.Forms.DialogResult.OK) return;
+            }
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "노트 데이터 파일 (*.notes)|*.notes";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                StreamReader sr = File.OpenText(ofd.FileNames[0]);
+
+                // 노트 데이터 읽어오기
+                if (sr.Peek() > 0)
+                {
+                    sb.Append(sr.ReadToEnd());
+                }
+
+                // 객체 닫기
+                sr.Close();
+
+                // 데이터 유무 예외처리
+                if (!sb.ToString().Contains("|"))
+                {
+                    System.Windows.Forms.MessageBox.Show("비어있는 노트 데이터 파일을 불러올 수 없습니다.", "Fatal Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // 데이터 파싱
+                //replay_Click(this, e);
+                NotesListBox.Items.Clear();
+                string[] note_datas = sb.ToString().Split('|');
+
+                foreach (string note_line in note_datas)
+                {
+                    // 마지막 공백 제거
+                    if (!note_line.Equals(""))
+                        NotesListBox.Items.Add(note_line);
+                }
+            }
+        }
+
+        // NAudio 라이브러리를 통한 mp3 파일 재생 함수
         private void Replay_Click(object sender, RoutedEventArgs e)
         {
+            // Scene 폼에 곡 이름, 아티스트 정보를 넘겨준다.
+            toscene(0, ArtistTextBox.Text + " - " + SongNameTextBox.Text,
+                (song.Lyrics.Length > 0) ? song.Lyrics[0] : null);
 
+            notesNowSelectedIndex = 0;
+            //formEffectNowSelectedIndex = 0;
+
+            // 노래 재생
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(SelectFile.Content.ToString());
+                outputDevice.Init(audioFile);
+            }
+
+            // audio file rewind
+            audioFile.Position = 0;
+
+            outputDevice.Play();
         }
 
         private void Pause_Click(object sender, RoutedEventArgs e)
         {
-
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(SelectFile.Content.ToString());
+                outputDevice.Init(audioFile);
+            }
+            outputDevice.Pause();
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
-
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(SelectFile.Content.ToString());
+                outputDevice.Init(audioFile);
+            }
+            outputDevice.Play();
         }
+
+        // timer
     }
 }
---
+++ LyricsSceneMaker_CSharp/LyricsSceneXaml.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LyricsSceneMaker_CSharp
+{
+    /// <summary>
+    /// Interaction logic for LyricsSceneXaml.xaml
+    /// </summary>
+    public partial class LyricsSceneXaml : Window
+    {
+        public LyricsSceneXaml()
+        {
+            InitializeComponent();
+        }
+
+        // this.dragmove 로 쉽게 창 드래그 기능을 구현할 수 있음
+        private void Titlebar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
